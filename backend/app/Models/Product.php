9404bd32b5466c2f90936530ff23014475ad3898
Rudrani Rachita Das <dasrudranirachita2006@gmail.com>
2026-09-10 18:44:45 +0530

    Create Product.php

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model {
+    protected $fillable = ['name', 'quantity', 'price', 'user_id'];
+    
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
